d9a4b5f5802ab809564635cbff26d1128c3e7631
sheep <sss3barry@gmail.com>
2017-06-11 21:08:26 +0800

    tests

--- pypathway/analysis/ora/__init__.py
+++ pypathway/analysis/ora/__init__.py
@@ -1,5 +1,5 @@
-# from ...analysis import ORASource, Analysis
-# from ...pathviz.query.network import NetworkMethod, NetworkRequest
+from ...analysis import ORASource, Analysis
+from ...pathviz.query.network import NetworkMethod, NetworkRequest
 
 from goatools.go_enrichment import GOEnrichmentStudy
 from goatools.obo_parser import GODag
@@ -8,43 +8,70 @@ from goatools.multiple_testing import Methods
 from goatools.pvalcalc import FisherFactory
 
 
-# class ORA(Analysis):
-#     @staticmethod
-#     def run(exp, pop=None, source=None, organism=None, cut_off=0.05, method=None):
-#         raise NotImplementedError()
-#
-#     def stat(self):
-#         raise NotImplementedError()
-#
-#
-# class Reactome(ORA):
-#     @staticmethod
-#     def run(exp, pop=None, source=None, custom_set=None, organism=None, cut_off=0.05, method=None):
-#         '''
-#         This method is implemented by the Reactome web server
-#
-#         :param exp: the list of over expressed gene symbol
-#         :param pop: the population
-#         :param source: the source, here default reactome
-#         :param organism: the organism, default is human
-#         :param cut_off: the cut off p value, default is 0.05
-#         :param method: the method is ora
-#         :return: a reactome ora object.
-#         '''
-#         # submit the analysis
-#         url = 'http://www.reactome.org/AnalysisService/identifiers/?interactors=false&pageSize=100&page=1&sortBy=ENTITIES_FDR&order=ASC&resource=TOTAL'
-#         file = '\n'.join([str(x) for x in exp])
-#         res = NetworkRequest(url, NetworkMethod.POST, post_arg={
-#             'data': file,
-#             'headers': {
-#                 'Content-Type': 'text/plain',
-#                 'Accept': 'application/json'
-#             }
-#         })
-#         print(res.text)
-#
-#     def stat(self):
-#         pass
+class ORA(Analysis):
+    @staticmethod
+    def run(exp, pop=None, source=None, organism=None, cut_off=0.05, method=None):
+        raise NotImplementedError()
+
+    def stat(self):
+        raise NotImplementedError()
+
+    @property
+    def table(self):
+        '''
+        The result of functional enrichment, should be in the format of pandas.Dataframes 
+        
+        :return: the table of analysis
+        '''
+        raise NotImplementedError()
+
+    @property
+    def main_property(self):
+        '''
+        The property used to plot in the bar chart, default is the -log2(columns)
+        
+        :return: 
+        '''
+        raise NotImplementedError()
+
+    @property
+    def network_data(self):
+        '''
+        The property provide the network for the viz class
+        
+        :return: 
+        '''
+        raise NotImplementedError()
+
+
+class Reactome(ORA):
+    @staticmethod
+    def run(exp, pop=None, source=None, custom_set=None, organism=None, cut_off=0.05, method=None):
+        '''
+        This method is implemented by the Reactome web server
+
+        :param exp: the list of over expressed gene symbol
+        :param pop: the population
+        :param source: the source, here default reactome
+        :param organism: the organism, default is human
+        :param cut_off: the cut off p value, default is 0.05
+        :param method: the method is ora
+        :return: a reactome ora object.
+        '''
+        # submit the analysis
+        url = 'http://www.reactome.org/AnalysisService/identifiers/?interactors=false&pageSize=100&page=1&sortBy=ENTITIES_FDR&order=ASC&resource=TOTAL'
+        file = '\n'.join([str(x) for x in exp])
+        res = NetworkRequest(url, NetworkMethod.POST, post_arg={
+            'data': file,
+            'headers': {
+                'Content-Type': 'text/plain',
+                'Accept': 'application/json'
+            }
+        })
+        print(res.text)
+
+    def stat(self):
+        pass
 
 
 class GO:
