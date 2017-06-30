376d172deb3f6f28efbd118fce5bf863993e47cd
sheep <sss3barry@gmail.com>
2017-06-30 08:02:36 +0800

    Migrate spia

--- pypathway/analysis/modelling/__init__.py
+++ pypathway/analysis/modelling/__init__.py
@@ -1,14 +1,26 @@
 # HOTNET, KeyPathwayMiner and MAGI
 from multiprocessing import Pool
 import subprocess
+import networkx as nx
+import re
+from collections import namedtuple
+
+
+MAGI_OUT = namedtuple('MAGI', ['num', 'genes', 'args'])
+
 
 class MAGI:
     '''
     The parallel warper of MAGI (Merge Affected Genes into Integrated networks)
     site: https://eichlerlab.gs.washington.edu/MAGI/
 
+    ToDO lists: 1. fill the output result in the parser
+                2. try to add information to the node
+                3. add the overview of the graph
+
     '''
     EXECUTABLE_PATH = '/Users/yangxu/Library/Developer/Xcode/DerivedData/MAGI-cmkybopnjlqlcidnrzkxmmycmuop/Build/Products/Debug/MAGI'
+    CLUSTER_PATH = '/Users/yangxu/Library/Developer/Xcode/DerivedData/MAGI-cmkybopnjlqlcidnrzkxmmycmuop/Build/Products/Debug/Cluster'
     TEST_ARGS = {
         'PPI Network': ['-p', '/Volumes/Data/magi/StringNew_HPRD.txt'],
         'cases mutation list': ['-c', '/Volumes/Data/magi/ID_2_Autism_4_Severe_Missense.Clean_WithNew.txt'],
@@ -18,12 +30,32 @@ class MAGI:
         'Length of genes': ['-l', '/Volumes/Data/magi/Gene_Name_Length.txt'],
         'run id': ['-i', 'test_run']
     }
+    CLUSTER_ARGS = {
+        'PPI Network': ['-p', '/Volumes/Data\ 1/magi/StringNew_HPRD.txt'],
+        'scores': ['-c', 'RandomGeneList.0'],
+        'Gene CoExpression Id': ['-h', '/Volumes/Data\ 1/magi/GeneCoExpresion_ID.txt'],
+        'CoExpression Matrix': ['-e', '/Volumes/Data\ 1/magi/adj1.csv.Tab.BinaryFormat'],
+        'Seed File': ['-s', 'seeds'],
+        'upper bound on control mutations': ['-m', '1'],
+        'lower bound on the size of the module': ['-l', '5'],
+        'upper bound on the size of the module': ['-u', '100'],
+        'minimum ratio of seed score allowed': ['-a', '0.5'],
+        'run id': ['-i', 'cluster'],
+        'minimum pair-wise coexpression value': ['-minCoExpr', '0.01'],   # optional
+        'minimum average coexpression of the module': ['-avgCoExpr', '0.415'],   # optional
+        'minimum density of PPI': ['-avgDensity', '0.08']
+    }
     additional = "-nc 5 -nt 1"
 
     @staticmethod
-    def make_pathway(args):
+    def make_pathway(*args):
+        '''
+        the warper of generate pathway seed, the test args is listed in the MAGI.TEST_ARGS
+
+        :param args:
+        :return:
+        '''
         colors, mutations = [8, 7, 6, 5], [4, 3, 2, 1]
-        # colors, mutations = [5, 6], [1, 2]
         pool = Pool(processes=1)
         for i in colors:
             for j in mutations:
@@ -35,22 +67,61 @@ class MAGI:
     def _run_color_and_mut(args, color, mutation):
         execute = MAGI.EXECUTABLE_PATH + " " + " ".join(
             ["{} {}".format(x[0], x[1]) for _, x in args.items()]) + " -nc {} -nm {}".format(color, mutation)
-        print(execute)
         r = subprocess.call(execute, shell=True)
         print(r)
 
     @staticmethod
-    def cluster():
-        pass
+    def cluster(*args):
+        print(args)
+        execute = MAGI.CLUSTER_PATH + ' ' + ' '.join(
+            ['{} {}'.format(x[0], x[1]) for x in args]
+        )
+        print(execute)
+        r = subprocess.check_output(execute, shell=True)
+        print(r)
+        # open the ppi network
+        for x in args:
+            if x[0] == '-p':
+                with open(x[1]) as fp:
+                    con = fp.read()
+                    G = nx.Graph()
+                    for x in con.split('\n'):
+                        if not x:
+                            continue
+                        G.add_edge(x.split('\t')[0], x.split('\t')[1])
+                    break
+        else:
+            raise Exception("No PPI network find")
+        return MAGI.parse_result(G, r.decode('utf8'))
 
     @staticmethod
     def run_test():
         pass
 
+    @staticmethod
+    def generate_seeds():
+        pass
+
+    @staticmethod
+    def parse_result(G, res):
+        '''
+        This function parse the output by the MAGI
+
+        :param G: The PPI network, we return the subnet from the PPI network
+        :param res: the output of MAGI
+        :return: a list of networkx Graph of the subnet
+        '''
+        results = re.findall("\d+.+?\d+ \d+ \d+ \d+ [\.\d]+ [\.\d]+ [\.\d]+", res, re.DOTALL)
+        res = []
+        for x in results:
+            m = MAGI_OUT(x.split('\n')[0], x.split('\n')[1: -1], x.split('\n')[-1].split(' '))
+            res.append(nx.subgraph(G, m.genes))
+        return res
+
 
 if __name__ == '__main__':
     # MAGI._run_color_and_mut(MAGI.TEST_ARGS, 5, 1)
-    MAGI.make_pathway(MAGI.TEST_ARGS)
+    MAGI.cluster(*list(MAGI.CLUSTER_ARGS.values()))
 
 
 class Hotnet2:
--- pypathway/analysis/network/__init__.py
+++ pypathway/analysis/network/__init__.py
@@ -1,13 +1,21 @@
-import pandas as pd
 import subprocess
 from io import StringIO
-from ...pathviz.query.common import NetworkMethod, NetworkRequest
 from ...analysis import Analysis, EnrichmentResult
 import re
 from ...pathviz.utils import plot_json
-import math
 import requests
+import json
+
+import numpy as np
+import os
+import pandas as pd
+from scipy import stats
+import random
 import time
+import math
+from statsmodels.sandbox.stats import multicomp
+import pickle
+from scipy.stats import norm
 
 
 class Enrichnet(Analysis, EnrichmentResult):
@@ -85,7 +93,6 @@ class Enrichnet(Analysis, EnrichmentResult):
         # print(self.basic_config)
         return plot_json(self.basic_config)
 
-
     def overview(self):
         raise NotImplementedError()
 
@@ -105,7 +112,207 @@ class Enrichnet(Analysis, EnrichmentResult):
         raise NotImplementedError()
 
 
-class SPIA(Analysis):
+class SPIA(Analysis, EnrichmentResult):
+    '''
+    The migration of SPIA from R to py
+
+    > res=spia(de=DE_Colorectal,all=ALL_Colorectal,organism="hsa",nB=2000,plots=FALSE,beta=NULL,combine="fisher",verbose=FALSE)
+
+    '''
+    @staticmethod
+    def _load_data(path):
+        datpT, id2name = {}, {}
+        for d in os.listdir(path):
+            if not '_' in d or d == '.DS_Store':
+                continue
+            id, name = d.split('_')[0], d.split('_')[1]
+            id2name[id] = name
+            datpT[id] = {}
+            for x in os.listdir(path + d):
+                fn = x[1:].replace('*', '/')
+                tb = pd.read_table(path + d + '/' + x, sep=' ')
+                datpT[id][fn + 'table'] = tb
+                datpT[id][fn] = tb.as_matrix()
+        return datpT, id2name
+
+    @staticmethod
+    def _load_de(de_path, all_path):
+        with open(de_path) as fp:
+            con = fp.read()
+        de = {}
+        for x in con.split("\n")[1:]:
+            if not x:
+                continue
+            de[int(x.split(' ')[0].replace('"', ''))] = float(x.split(' ')[1])
+        with open(all_path) as fp:
+            con = fp.read()
+        all = [int(x.split(' ')[1].replace('"', '')) for x in con.split('\n')[1:] if x]
+        return de, all
+
+    @staticmethod
+    def save_data(dataT_all, id2name):
+        with open(os.path.dirname(os.path.realpath(__file__)) + "/spia_data/hsa", 'wb') as fp:
+            pickle.dump([dataT_all, id2name], fp)
+
+    @staticmethod
+    def load_dumped_data(organism):
+        with open(os.path.dirname(os.path.realpath(__file__)) + "/spia_data/{}".format(organism), 'rb') as fp:
+            return pickle.load(fp)
+
+    @staticmethod
+    def load_json_data(organism):
+        '''
+        load json data from certain organism.
+
+        :param organism:  the organism, this method will load the cor data
+        :return: the loaded data
+        '''
+        with open(os.path.dirname(os.path.realpath(__file__)) + "/spia_data/{}.json".format(organism)) as fp:
+            data = json.load(fp)
+        datpT = {}
+        id2name = data['id2name']
+        for pid, v in data.items():
+            if pid == 'id2name':
+                continue
+            datpT[pid] = {}
+            for key, d in v.items():
+                if key == 'row_names':
+                    datpT[pid][key] = d
+                else:
+                    m = np.zeros((len(v['row_names']), len(v['row_names'])))
+                    for x in d:
+                        m[x[0]][x[1]] = 1
+                    datpT[pid][key] = m
+        return datpT, id2name
+
+    @staticmethod
+    def run(de, all, organism='hsa', nB=2000, beta=None, combine='fisher'):
+        if organism not in ['hsa', 'mmu']:
+            raise Exception("The organism not contained in the prepared data")
+        de, all = SPIA._load_de(de, all)
+        datpT_ALL, id2name = SPIA.load_json_data(organism)
+        rel = ["activation", "compound", "binding/association", "expression", "inhibition",
+               "activation_phosphorylation", "phosphorylation", "inhibition_phosphorylation",
+               "inhibition_dephosphorylation", "dissociation", "dephosphorylation",
+               "activation_dephosphorylation", "state change", "activation_indirect effect",
+               "inhibition_ubiquination", "ubiquination", "expression_indirect effect",
+               "inhibition_indirect effect", "repression", "dissociation_phosphorylation",
+               "indirect effect_phosphorylation", "activation_binding/association",
+               "indirect effect", "activation_compound", "activation_ubiquination"]
+        inter_value = [1, 0, 0, 1, -1, 1, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0, 1, -1, -1, 0, 0, 1, 0, 1,
+                       1] or beta
+        rel_dict = {rel[i]: inter_value[i] for i in range(len(rel))}
+        datp_ALL = {}
+        for k, v in datpT_ALL.items():
+            sizem = len(v[rel[0]][0])
+            s, con = np.zeros((sizem, sizem)), np.zeros((sizem, sizem))
+            for kk, vv in rel_dict.items():
+                con += v[kk] * abs(vv)
+                s += v[kk] * vv
+            zz = np.reshape(np.repeat(con.sum(axis=0), sizem), (sizem, sizem))
+            z = np.transpose(zz)
+            z[z == 0] = -1
+            r = np.divide(s, z)
+            datp_ALL[k] = r
+        smPFS, tAraw, tA, pNDE, pb, pG, status = {}, {}, {}, {}, {}, {}, {}
+        # calculate the Ac
+        for k, v in datp_ALL.items():
+            row_names = datpT_ALL[k]['row_names']
+            # let first calculate the pNDE
+            noMy = len(set(row_names) & set(de.keys()))
+            pNDE[k] = stats.hypergeom.sf(noMy - 1, len(all), len(set(row_names) & set(all)), len(de))
+            # then calculate the Ac and pPERT
+            M = np.eye(v.shape[0]) * -1 + v
+            if np.linalg.det(M) == 0:
+                smPFS[k], tAraw[k], tA[k], pb[k] = np.nan, np.nan, np.nan, np.nan
+                continue
+            X = []
+            for x in row_names:
+                if x in de:
+                    X.append(de[x])
+                else:
+                    X.append(0)
+            pfs = np.linalg.solve(M, -np.array(X))
+            smPFS[k] = sum(pfs - X)
+            tAraw[k] = smPFS[k]
+            pfstmp = []
+            de_sample = list(de.values())
+            all_sample = [i for i, x in enumerate(row_names) if x in all]
+            length = len(X)
+            for i in range(2000):  # nB
+                x = np.zeros(length)
+                sp = random.sample(de_sample, noMy)
+                idx = random.sample(all_sample, noMy)
+                x[idx] = sp
+                tt = np.linalg.solve(M, -x)
+                pfstmp.append(sum(tt - x))
+            tA[k] = tAraw[k] - np.median(np.array(pfstmp))
+            if tA[k] > 0:
+                status[k] = "Activated"
+            else:
+                status[k] = "Inhibited"
+            ob = tA[k]
+            pfstmp = np.array(pfstmp) - np.median(np.array(pfstmp))
+            if ob > 0:
+                pb[k] = sum([1 for pf in pfstmp if pf >= ob]) / len(pfstmp) * 2
+                if pb[k] <= 0:
+                    pb[k] = 1 / nB / 100
+                elif pb[k] >= 1:
+                    pb[k] = 1
+            elif ob < 0:
+                pb[k] = sum([1 for pf in pfstmp if pf <= ob]) / len(pfstmp) * 2
+                if pb[k] <= 0:
+                    pb[k] = 1 / nB / 100
+                elif pb[k] >= 1:
+                    pb[k] = 1
+            else:
+                pb[k] = 1
+            if combine == 'fisher':
+                c = pNDE[k] * pb[k]
+                pG[k] = c - c * math.log(c)
+            else:
+                # comb = pnorm((qnorm(p1) + qnorm(p2)) / sqrt(2))
+                pG[k] = norm.cdf(norm.ppf(pNDE[k]) + norm.ppf(pb[k]) / math.sqrt(2))
+            # print('id: ', k, '\ttA:', tA[k], '\tpNDE: ', pNDE[k], '\t pPERT: ', pb[k], '\tPG: ', pG[k])
+        _, o, _, _ = multicomp.multipletests(list(pG.values()), method='fdr_bh')
+        pGfdr = {list(pG.keys())[i]: o[i] for i in range(len(list(pG.keys())))}
+        _, o, _, _ = multicomp.multipletests(list(pNDE.values()), method='fdr_bh')
+        pNDEfdr = {list(pNDE.keys())[i]: o[i] for i in range(len(list(pNDE.keys())))}
+        _, o, _, _ = multicomp.multipletests(list(pG.values()), method='bonferroni')
+        pGbf = {list(pG.keys())[i]: o[i] for i in range(len(list(pG.keys())))}
+        df = pd.DataFrame([id2name, pNDE, pb, pG, pGfdr, pNDEfdr, pGbf, status]).T
+        df.columns = ['name', 'pNDE', 'pPERT', 'pG', 'pGfdr', 'pNDEfdr', 'pGbf', 'status']
+        df = df.sort_values(by='pGfdr')
+        return SPIA(df, de, all, organism, nB, beta, combine)
+
+    def __init__(self, df, de, all, organism, nB, beta, combine):
+        EnrichmentResult.__init__(self, df, de, 'KEGG', 'SPIA', 'pGfdr')
+        self.df, self.de, self.all, self.organism, self.nB, self.beta, self.combine = df, de, all, organism, nB, beta, combine
+
+    @property
+    def table(self):
+        return self.df
+
+    @property
+    def plot(self, count=15):
+        self.basic_config['yAxis']['data'] = []
+        self.basic_config['series'][0]['data'] = []
+        self.basic_config['title']['subtext'] = self.target
+        self.basic_config['title']['text'] = "{} Enrichment Analysis".format(self.method.upper())
+        candidate = []
+        for x in self.df.iterrows():
+            candidate.append([x[1][0], -math.log2(x[1][4])])
+        candidate = sorted(candidate, key=lambda x: x[1], reverse=True)
+        for x in candidate[:15]:
+            self.basic_config['yAxis']['data'].append(x[0])
+            self.basic_config['series'][0]['data'].append(x[1])
+        # print(self.basic_config)
+        return plot_json(self.basic_config)
+
+
+
+
+class SPIA_C(Analysis):
     '''
     The adaptor of C implement of Signaling Pathway Impact Analysis (SPIA)
 
